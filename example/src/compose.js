c668e93df3bf2e42411ea0a69ba46594eb26ad2c
ryota-murakami <dojce1048@gmail.com>
2018-11-16 02:54:22 +0900

    not work

--- example/src/compose.js
+++
@@ -1,9 +0,0 @@
-/**
- * recompose v0.30.0 (https://github.com/acdlite/recompose)
- * Copyright (c) 2015-2018 Andrew Clark
- * The MIT License (MIT)(https://github.com/acdlite/recompose/blob/master/LICENSE.md)
- */
-const compose = (...funcs) =>
-  funcs.reduce((a, b) => (...args) => a(b(...args)), arg => arg)
-
-export default compose
--- example/src/lib/index.js
+++
@@ -1,4 +0,0 @@
-import Provider from './Provider'
-import StoreContext from './StoreContext'
-
-export { Provider, StoreContext }
