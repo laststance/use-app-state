2a21f18de7a9564d5623e90a9768ef130c992d6e
ryota-murakami <dojce1048@gmail.com>
2018-11-22 02:32:49 +0900

    expand integration.test

--- src/integration.test.js
+++ src/integration.test.js
@@ -65,4 +65,15 @@ test('should update json value by setStore()', () => {
   expect(stableNode).toHaveTextContent(`{"blank":"blank"}`)
   fireEvent.click(stableNode)
   expect(stableNode).toHaveTextContent(`{"changed":"changed"}`)
+
+  // repeat 'should count increments by setStore()'
+  const countNode = getByTestId('count')
+  expect(countNode).toHaveTextContent('0')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('1')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('2')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('3')
+  fireEvent.click(countNode)
 })
