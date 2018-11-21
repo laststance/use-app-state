024ab15a891ab8f10080986e0bb545f8fe380495
ryota-murakami <dojce1048@gmail.com>
2018-11-22 02:29:55 +0900

    added integration.test

--- src/integration.test.js
+++ src/integration.test.js
@@ -9,17 +9,24 @@ afterEach(cleanup)
 const Receiver = () => {
   const { store, setStore } = useContext(StoreContext)
 
-  const handleClick = () => setStore({ count: store.count + 1 })
+  const clickCount = () => setStore({ count: store.count + 1 })
+
+  const clickStable = () => setStore({ stable: { changed: 'changed' } })
 
   return (
-    <button data-testid="button" onClick={handleClick}>
-      {store.count}
-    </button>
+    <div>
+      <button data-testid="count" onClick={clickCount}>
+        {store.count}
+      </button>
+      <button data-testid="stable" onClick={clickStable}>
+        {JSON.stringify(store.stable)}
+      </button>
+    </div>
   )
 }
 
 const App = () => {
-  const initialState = { count: 0 }
+  const initialState = { count: 0, stable: { blank: 'blank' } }
 
   return (
     <Provider initialState={initialState}>
@@ -28,29 +35,34 @@ const App = () => {
   )
 }
 
-test('Privider is sharing store value to bottom component', () => {
+test('Provider is sharing store value to bottom component', () => {
   const { getByTestId } = render(<App />)
 
-  const node = getByTestId('button')
+  const node = getByTestId('count')
   expect(node).toHaveTextContent('0')
 })
 
-test('working fine setStore() API', () => {
+test('should count increments by setStore()', () => {
   const { getByTestId } = render(<App />)
-  const node = getByTestId('button')
-  expect(node).toHaveTextContent('0')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('1')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('2')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('3')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('4')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('5')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('6')
-  fireEvent.click(node)
-  expect(node).toHaveTextContent('7')
+  const countNode = getByTestId('count')
+  expect(countNode).toHaveTextContent('0')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('1')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('2')
+  fireEvent.click(countNode)
+  expect(countNode).toHaveTextContent('3')
+  fireEvent.click(countNode)
+
+  // stable is still untouch above tests, so confirm state is initialValue
+  const stableNode = getByTestId('stable')
+  expect(stableNode).toHaveTextContent(`{"blank":"blank"}`)
+})
+
+test('should update json value by setStore()', () => {
+  const { getByTestId } = render(<App />)
+  const stableNode = getByTestId('stable')
+  expect(stableNode).toHaveTextContent(`{"blank":"blank"}`)
+  fireEvent.click(stableNode)
+  expect(stableNode).toHaveTextContent(`{"changed":"changed"}`)
 })
