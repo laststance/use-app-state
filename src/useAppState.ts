e67e9fbcf65211feef51f14aefb9e109e29571f3
ryota-murakami <dojce1048@gmail.com>
2019-12-01 16:30:38 +0900

    bump: 1.2.1 fix type error

--- src/useAppState.ts
+++ src/useAppState.ts
@@ -1,7 +1,7 @@
-import { SetStateAction, useContext } from 'react'
+import { Dispatch, SetStateAction, useContext } from 'react'
 import Context from './Context'
 
-export default function useAppState<S>(): [S, SetStateAction<S>] {
+export default function useAppState<S>(): [S, Dispatch<SetStateAction<S>>] {
   const { appState, setAppState } = useContext(Context)
 
   return [appState, setAppState]
