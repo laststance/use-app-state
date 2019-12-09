18762515e783782a1c51ea02768e604f1f880930
ryota-murakami <dojce1048@gmail.com>
2019-12-10 01:18:57 +0900

    useAppState.ts: add comment of intention

--- src/useAppState.ts
+++ src/useAppState.ts
@@ -4,5 +4,6 @@ import Context from './Context'
 export default function useAppState<S>(): [S, (appState: S) => void] {
   const { appState, setAppState } = useContext(Context)
 
+  // Make exact same signature as useState() https://reactjs.org/docs/hooks-reference.html#basic-hooks
   return [appState, setAppState]
 }
